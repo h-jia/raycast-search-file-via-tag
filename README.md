18f775b04e96592f329bd8b6e022b8963b3088a2
h-jia <h.jia.cam@gmail.com>
2024-04-05 12:04:06 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# <p align="center"> Raycast Search Files By Tag</p>
+
+## Install
+
+- Install nodejs.
+- Run `npm install && npm run dev`
+
+## Use
+
+- `[tag name] + space + [search content]`
+- Hit Enter to open.
---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+import { ActionPanel, List, Action, open } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { useExec } from "@raycast/utils";
+import path from "path";
+
+export default function Command() {
+  const [searchContent, setSearchContent] = useState(["", ""]);
+  const [files, setFiles] = useState<string[]>([]);
+
+  const { data } = useExec(`mdfind 'tag:${searchContent[0] === "" ? "ZZZZZZ" : searchContent[0]}'`, {
+    shell: true,
+    keepPreviousData: true,
+  });
+
+  useEffect(() => {
+    if (data) {
+      let files = data.split(/\r?\n/);
+      if (searchContent[1]) {
+        const searchTerm = searchContent[1].toLowerCase().replace(/[-_]/g, '');
+        files = files.filter((item) =>
+          item.toLowerCase().replace(/[-_]/g, '').includes(searchTerm)
+        );
+      }
+      setFiles(files);
+    }
+  }, [data, searchContent[1]]);
+
+  const updateSearch = (str: string) => {
+    const [tag, content] = str.split(" ");
+    setSearchContent([tag, content]);
+  };
+
+  const formatFileName = (fileName: string) => {
+    const baseName = path.basename(fileName);
+    const cleanedBaseName = baseName
+      .replace(/[-_]/g, ' ')
+      .replace(/\.pdf$/, '')
+      .replace(/\s+main$/, '');
+    const words = cleanedBaseName.split(' ');
+    const filteredWords = words.filter((word) => word.length <= 20);
+    const formattedName = filteredWords.join(' ').replace(/\s+/g, ' ').trim();
+    return formattedName;
+  };
+
+  return (
+    <List filtering={false} onSearchTextChange={updateSearch}>
+      {files.map((item) => (
+        <List.Item
+          key={item}
+          id={item}
+          title={`[${searchContent[0]}] ${formatFileName(item)}`}
+          actions={
+            <ActionPanel>
+              <Action title="Open File" onAction={() => open(item)} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
